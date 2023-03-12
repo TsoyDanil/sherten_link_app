925b946113bc7eb9d752ba1e1cae37d0c7d8ad1b
danil tsoy <abdullaevdanil2002@gmail.com>
2023-03-12 18:51:41 +0600

    #1 #4 changed some server settings to enable it to work everywhere

--- backend/src/index.config.ts
+++ backend/src/index.config.ts
@@ -3,5 +3,5 @@ import path from 'path'
 export const config = {
     port: 8000,
     filePath: path.join(__dirname, '../public/uploads'),
-    mongoServerName: 'myLinks'
+    mongoServerName: 'TsoyDanilLinks'
 }
--- backend/src/repository/mongoDB.ts
+++ backend/src/repository/mongoDB.ts
@@ -1,5 +1,6 @@
 import { MongoClient, Db } from 'mongodb'
 import dotenv from 'dotenv'
+import { config } from '../index.config'
 dotenv.config()
 
 export class MongoDB {
@@ -7,7 +8,7 @@ export class MongoDB {
     private db: Db
     constructor(){
         this.client = new MongoClient(process.env.MONGO_CLIENT_URL || '')
-        this.db = this.client.db('myLinks')
+        this.db = this.client.db(config.mongoServerName)
     }
     
     public getDB = (): Db => {
